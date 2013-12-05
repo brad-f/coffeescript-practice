f84677952f5205f1a764874aa964a158db8b8b21
Brad Fitzwater <brad.fitzwater@gmail.com>
2013-12-04 20:48:24 -0500

    added package.json, some utility scripts, moved some files

---
+++ env/bootstrap.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install -y python-software-properties python g++ make
+sudo add-apt-repository -y ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install -y nodejs
+
