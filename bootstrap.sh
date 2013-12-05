ddd2e305d8c838b976279cc23ce59fd26031e063
Brad Fitzwater <brad.fitzwater@gmail.com>
2013-12-04 20:50:56 -0500

    moved some files

--- bootstrap.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get update
-sudo apt-get install -y python-software-properties python g++ make
-sudo add-apt-repository -y ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get install -y nodejs
-sudo npm install -g coffee-script
-
--- tools.sh
+++
@@ -1,4 +0,0 @@
-sudo apt-get install -y vim
-touch ~/.vimrc
-echo ":set expandtab" >> ~/.vimrc
-echo ":set tabstop=2" >> ~/.vimrc
